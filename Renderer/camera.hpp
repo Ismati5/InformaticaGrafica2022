1501d86766969368ded5640a5212e584f202ba6e
Ismati5 <796919@unizar.es>
2022-11-06 01:15:59 +0100

    Primer test de sombras

--- Renderer/camera.hpp
+++ Renderer/camera.hpp
@@ -84,12 +84,34 @@ public:
         // cout << "pixelSize_y: " << pixelSize_y << endl;
     }
 
+    void foo() {}
+
+    /**
+     * @brief Returns the color
+     *
+     * @param x
+     * @param wi
+     * @param w0
+     * @param kd
+     * @return Vect3
+     */
     Vect3 fr(Point x, Direction wi, Direction w0, Vect3 kd)
     {
         return kd / PI;
     }
 
-    void colorValue(Vect3 &emission, Point x, Direction w0, vector<Light *> light_points, Direction n, Vect3 color)
+    /**
+     * @brief Sets emission property taking into count shadows
+     *
+     * @param objs
+     * @param emission
+     * @param x
+     * @param w0
+     * @param light_points
+     * @param n
+     * @param color
+     */
+    void colorValue(vector<Object *> objs, Vect3 &emission, Point x, Direction w0, vector<Light *> light_points, Direction n, Vect3 color)
     {
 
         Vect3 aux_emission;
@@ -97,47 +119,61 @@ public:
         float aux2;
         aux_emission = Vect3(0, 0, 0);
 
+        Direction sur_normal;
+        Point aux_x;
+        Direction wi;
+        Ray shadow;
+        bool isShadow = false;
+        float t1 = 0;
+
         for (Light *light : light_points)
         {
 
-            // Left term (Li)
-            aux_emission = light->power / ((x - light->center).modulus() * (x - light->center).modulus());
-            // cout << "aux_EMISION_1: " << aux_emission << endl;
+            // Check if it's a shadow
+            wi = (light->center - x).normalize();
+            shadow.d = wi;
+            shadow.p = x;
 
-            // Middle term (fr)
-            Direction wi = (light->center - x).normalize();
-            aux = fr(x, wi, w0, color);
-            aux_emission *= aux;
-            // cout << "aux: " << aux << endl;
-            // cout << "aux_EMISION_2: " << aux_emission << endl;
+            for (auto i : objs)
+            {
+                if (i->intersect(shadow, t1, sur_normal, aux_x))
+                {
+                    if (t1 > 0.00001 && t1 < (light->center - x).modulus())
+                    {
+                        isShadow = true;
+                        break;
+                    }
+                }
+            }
 
-            // Right term
+            // If it's not a shadow
+            if (!isShadow)
+            {
+                // Left term (Li)
+                aux_emission = light->power / ((x - light->center).modulus() * (x - light->center).modulus());
+                // cout << "aux_EMISION_1: " << aux_emission << endl;
 
-            //  cout << "Normal: " << n << endl;
-            //  cout << "Light center: " << light->center - x << endl;
+                // Middle term (fr)
+                aux = fr(x, wi, w0, color);
+                aux_emission *= aux;
+                // cout << "aux: " << aux << endl;
+                // cout << "aux_EMISION_2: " << aux_emission << endl;
 
-            aux2 = abs(n.dotProd((light->center - x).normalize()));
-            aux_emission *= aux2;
-            //  cout << "aux2: " << aux2 << endl << endl;
-            // cout << "aux_EMISION_3: " << aux_emission << endl;
+                // Right term
 
-            emission += aux_emission;
+                //  cout << "Normal: " << n << endl;
+                //  cout << "Light center: " << light->center - x << endl;
+
+                aux2 = abs(n.dotProd((light->center - x).normalize()));
+                aux_emission *= aux2;
+                //  cout << "aux2: " << aux2 << endl << endl;
+                // cout << "aux_EMISION_3: " << aux_emission << endl;
+
+                emission += aux_emission;
+            }
         }
 
         // cout << "EMISION: " << emission << endl;
-
-        if (emission.x > 255)
-        {
-            emission.x = 255;
-        }
-        if (emission.y > 255)
-        {
-            emission.y = 255;
-        }
-        if (emission.z > 255)
-        {
-            emission.z = 255;
-        }
     }
 
     /**
@@ -152,11 +188,9 @@ public:
     {
 
         // file header
-        ofstream file("renders/" + outfile);
-        file << "P3" << endl;
-        // file << "# " << outfile << endl;
-        file << size[0] << " " << size[1] << endl;
-        file << "255" << endl;
+
+        vector<vector<Vect3>> content;
+        int max_emission = 0;
 
         Ray ray;
         ray.p = origin;
@@ -173,8 +207,11 @@ public:
 
         for (float i = 0; i < size[1]; i++) // i rows
         {
+            vector<Vect3> content_aux;
+
             for (float j = 0; j < size[0]; j++) // j columns
             {
+
                 for (float r = 0; r < rays_per_pix; r++)
                 {
 
@@ -186,28 +223,28 @@ public:
                     float ray2_x = ((float(rand()) / float(RAND_MAX)) * pixelSize_y.x) - pixelSize_y.x / 2;
                     float ray2_y = ((float(rand()) / float(RAND_MAX)) * pixelSize_y.y) - pixelSize_y.y / 2;
                     float ray2_z = ((float(rand()) / float(RAND_MAX)) * pixelSize_y.z) - pixelSize_y.z / 2;
-                    Direction variation_y(ray_x, ray_y, ray_z);
+                    Direction variation_y(ray2_x, ray2_y, ray2_z);
 
                     Point pixel = topLeft - pixelSize_x * j - pixelSize_y * i - pixelSize_x / 2 - pixelSize_y / 2 + variation_x + variation_y;
 
                     // cout << "[" << i << "]" << "[" << j << "] " << pixel << endl;
                     ray.d = (pixel - origin).normalize();
 
-                    // check sphere intersections
+                    // check intersections
                     for (auto i : objs)
                     {
 
                         if (i->intersect(ray, t1, sur_normal, x))
                         {
 
-                            if (t1 > 0 && t1 < lowest_t1)
+                            if (t1 > 0.00001 && t1 < lowest_t1)
                             {
                                 lowest_t1 = t1;
 
                                 Direction w0 = (origin - x).normalize();
                                 closest_emission = Vect3(0, 0, 0);
-                                colorValue(closest_emission, x, w0, light_points, sur_normal, i->emission);
-                                // closest_emission = i->emission; // Without pathtracing
+                                colorValue(objs, closest_emission, x, w0, light_points, sur_normal, i->emission); // With path tracing
+                                // closest_emission = i->emission; // Without path tracing
                                 intersected = true;
                             }
                         }
@@ -225,7 +262,14 @@ public:
                 {
                     total_emission /= intersections;
 
-                    file << total_emission.x << " " << total_emission.y << " " << total_emission.z << "    ";
+                    if (max_emission < round(total_emission.x))
+                        max_emission = round(total_emission.x);
+                    if (max_emission < round(total_emission.y))
+                        max_emission = round(total_emission.y);
+                    if (max_emission < round(total_emission.z))
+                        max_emission = round(total_emission.z);
+
+                    content_aux.push_back(Vect3(round(total_emission.x), round(total_emission.y), round(total_emission.z)));
 
                     intersections = 0;
 
@@ -234,10 +278,27 @@ public:
                 }
                 else
                 {
-                    file << 0 << " " << 0 << " " << 0 << "    ";
+                    content_aux.push_back(Vect3(0, 0, 0));
                 }
             }
-            progressBar(i, size[0], start);
+
+            content.push_back(content_aux);
+
+            progressBar(i, size[1], start);
+        }
+
+        ofstream file("renders/" + outfile);
+        file << "P3" << endl;
+        file << "#MAX=255" << endl;
+        file << size[0] << " " << size[1] << endl;
+        file << max_emission << endl;
+
+        for (float i = 0; i < size[1]; i++) // i rows
+        {
+            for (float j = 0; j < size[0]; j++) // j columns
+            {
+                file << content.at(i).at(j).x << " " << content.at(i).at(j).y << " " << content.at(i).at(j).z << "    ";
+            }
             file << endl;
         }
 
--- Renderer/render.cpp
+++ Renderer/render.cpp
@@ -28,13 +28,18 @@ Vect3 yellow = Vect3(233, 236, 107);
 Vect3 blue = Vect3(153, 255, 255);
 Vect3 green = Vect3(119, 221, 119);
 Vect3 gray = Vect3(207, 207, 196);
+Vect3 light_grey = Vect3(238, 238, 238);
 Vect3 purple = Vect3(177, 162, 202);
 Vect3 orange = Vect3(239, 190, 125);
 Vect3 white = Vect3(255, 255, 255);
 
+Vect3 yellow_backroom_floor = Vect3(198, 197, 139);
+Vect3 yellow_backroom_wall = Vect3(228, 230, 168);
+Vect3 yellow_backroom_ceiling = Vect3(225, 226, 187);
 
-int loadOBJfile(Triangle triangles[10000], string fileName, Vect3 emi) {
-    
+int loadOBJfile(Triangle triangles[10000], string fileName, Vect3 emi)
+{
+
     ifstream file(fileName);
 
     if (!file.is_open())
@@ -49,12 +54,12 @@ int loadOBJfile(Triangle triangles[10000], string fileName, Vect3 emi) {
     string line;
     while (getline(file, line))
     {
-        if (line.find("v ", 0) == 0) //Vertices
+        if (line.find("v ", 0) == 0) // Vertices
         {
             Point p;
             sscanf(line.c_str(), "v %f %f %f", &p.x, &p.y, &p.z);
             vertices.push_back(p);
-        } 
+        }
         else if (line.find("f ", 0) == 0) // Faces
         {
             Vect3 v;
@@ -64,22 +69,20 @@ int loadOBJfile(Triangle triangles[10000], string fileName, Vect3 emi) {
             if (matches == 3)
             {
                 faces.push_back(v);
-            } 
-            else if(matches == 4)
+            }
+            else if (matches == 4)
             {
                 faces.push_back(v);
                 Vect3 v2(v.x, v.z, w);
                 faces.push_back(v2);
             }
-
-        }       
+        }
     }
 
     // for (Vect3 i : faces)
     // {
     //     cout << i << endl;
     // }
-    
 
     int e = 0;
     for (Vect3 i : faces)
@@ -87,26 +90,30 @@ int loadOBJfile(Triangle triangles[10000], string fileName, Vect3 emi) {
         triangles[e].p1 = vertices.at(i.x - 1);
         triangles[e].p2 = vertices.at(i.y - 1);
         triangles[e].p3 = vertices.at(i.z - 1);
-        triangles[e].setEmission(emi);     
+        triangles[e].setEmission(emi);
         triangles[e].sertNormal();
-        //cout << triangles[e].normal << endl;
-        // Triangle triangle(
-        //     vertices.at(i.x - 1),
-        //     vertices.at(i.y - 1),
-        //     vertices.at(i.z - 1),
-        //     emi);
-        // objs.push_back(&triangles[e]);
+        // cout << triangles[e].normal << endl;
+        //  Triangle triangle(
+        //      vertices.at(i.x - 1),
+        //      vertices.at(i.y - 1),
+        //      vertices.at(i.z - 1),
+        //      emi);
+        //  objs.push_back(&triangles[e]);
         e++;
     }
 
     file.close();
 
     return faces.size();
-
 }
 
 void createRender(string file, int rays)
 {
+    vector<Object *> objs;
+    vector<Light *> lights;
+
+    /* (Para pruebas con .obj)
+
     Point o(0, 20, 0);
     Direction l(1, 0, 0);
     Direction u(0, 0, 1);
@@ -115,13 +122,11 @@ void createRender(string file, int rays)
 
     Camera camera(l, u, f, o, size);
 
-    vector<Light *> lights;
     Point l_c1(0, 25, 0);
     Vect3 l_p1 = Vect3(500, 500, 900);
     Light light1(l_c1, l_p1);
     lights.push_back(&light1);
 
-    vector<Object *> objs;
     Triangle triangles[30000];
 
     int numPolygons = loadOBJfile(triangles, "objs/DiamondSword.obj", blue);
@@ -130,36 +135,55 @@ void createRender(string file, int rays)
     {
         objs.push_back(&triangles[i]);
         // cout << triangles[i].p3 << endl;
-    }
+    }*/
+
+    int size[2] = {1920, 1080};
+
+    Point o(0, 0, -3.5);
+    Direction l(-1.7777, 0, 0);
+    Direction u(0, 1, 0);
+    Direction f(0, 0, 3);
+
+    Camera camera(l, u, f, o, size);
+
+    Point l_c1(0, 0.5, 0);
+    Vect3 l_p1 = Vect3(1, 0, 0);
+    Light light1(l_c1, l_p1);
+    lights.push_back(&light1);
+
+    Point l_c2(-0.5, 0, 0);
+    Vect3 l_p2 = Vect3(0, 1, 0);
+    Light light2(l_c2, l_p2);
+    lights.push_back(&light2);
 
     Direction n(1, 0, 0);
-    Plane left_plane(n, 1, orange);
-    //  objs.push_back(&left_plane);
+    Plane left_plane(n, 1, light_grey);
+    objs.push_back(&left_plane);
     Direction n1(-1, 0, 0);
-    Plane right_plane(n1, 1, yellow);
-    //  objs.push_back(&right_plane);
-    Direction n2(0, 0, 1);
-    Plane floor_plane(n2, 1, gray);
-    // objs.push_back(&floor_plane);
+    Plane right_plane(n1, 1, gray);
+    objs.push_back(&right_plane);
+    Direction n2(0, 1, 0);
+    Plane floor_plane(n2, 1, purple);
+    objs.push_back(&floor_plane);
     Direction n3(0, -1, 0);
     Plane ceiling_plane(n3, 1, red);
-    //  objs.push_back(&ceiling_plane);
+    objs.push_back(&ceiling_plane);
     Direction n4(0, 0, -1);
-    Plane back_plane(n4, 1, purple);
-    //  objs.push_back(&back_plane);
+    Plane back_plane(n4, 1, light_grey);
+    objs.push_back(&back_plane);
 
     Point c(-0.5, -0.7, 0.25);
-    Sphere left_sphere(c, 0.3, blue);
-    // objs.push_back(&left_sphere);
+    Sphere left_sphere(c, 0.3, white);
+    objs.push_back(&left_sphere);
     Point c1(0.5, -0.7, -0.25);
-    Sphere right_sphere(c1, 0.3, green);
-    // objs.push_back(&right_sphere);
+    Sphere right_sphere(c1, 0.3, blue);
+    objs.push_back(&right_sphere);
 
     Point p1(-0.5, -0.7, 0.25);
     Point p2(0, -0.7, 0.25);
     Point p3(-0.25, 0, 0.25);
     Triangle triangle(p1, p2, p3, red);
-    //objs.push_back(&triangle);
+    // objs.push_back(&triangle);
 
     camera.render(file, objs, rays, lights);
 }
